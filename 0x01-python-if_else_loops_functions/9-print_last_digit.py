c3f0641d8dccbe589e48b34201b305f1f2d91a12
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2022-12-27 15:13:30 -0800

    python

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    l = number % 10
-    print('{}'.format(l), end="")
-    return l
+    print('{}'.format(abs(number) % 10), end="")
+    return abs(number) % 10
