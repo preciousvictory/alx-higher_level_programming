1fad4d459a4188baa4255e786efcdf916b1fed08
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-01-02 11:03:17 -0800

    python

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Square that defines a square by: (based on 0-square.py)"""
+
+
+class Square:
+    """a class Square that defines a square by"""
+    def __init__(self, size):
+        """A new square
+
+        Args:
+            size: the size of new square
+            Private instance attribute: size, Instantiation with size
+
+        """
+        self.__size = size
