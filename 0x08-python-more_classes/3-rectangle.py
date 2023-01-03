b12218232e8d15ceafb5cd95318d44317df457d7
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-01-02 19:38:03 -0800

    python

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -52,7 +52,6 @@ class Rectangle:
         rect = []
         for i in range(self.height):
             [rect.append('#') for j in range(self.__width)]
-            #rect.append('#'*self.width)
             if i != self.__height - 1:
                 rect.append("\n")
 
