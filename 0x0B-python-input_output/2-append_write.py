46ca417a6fccc2bc43a819cafcec75a6a62ae474
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-01-10 18:11:46 -0800

    python

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,8 +6,8 @@ appends a string at the end of a text file
 
 def append_write(filename="", text=""):
     """
-    a function that appends a string at the end of a text file (UTF8) and returns the
-    number of characters added:
+    a function that appends a string at the end of a text file (UTF8) and
+    returns the number of characters added:
     """
     with open(filename, 'a', encoding="utf-8") as f:
         return f.write(text)
