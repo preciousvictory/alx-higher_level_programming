1fe98b30af49023310d73834b25a0f7c5c300168
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-01-10 18:22:26 -0800

    python

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+"""
+the JSON representation of an object (string)
+"""
+
+
+def to_json_string(my_obj):
+    """
+    a function that returns the JSON representation of an object (string):
+    """
+    return json.dumps(my_obj)
