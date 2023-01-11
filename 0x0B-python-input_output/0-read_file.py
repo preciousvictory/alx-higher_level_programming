b0c79d83dfdf758012487a612daa95cba0f1b1be
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-01-10 17:49:56 -0800

    python

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+reads a text file 
+"""
+
 
 def read_file(filename=""):
+    """
+    a function that reads a text file (UTF8) and prints it to stdout
+    """
     with open(filename, encoding="utf-8") as f:
-        for line in f:
-            print(line, end="")
+        print(f.read(), end="")
