686aa38239d3b7e9d68dfadaa144a1e44e88cac5
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-01-10 21:16:56 -0800

    python

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""returns the dictionary description with simple data structure """i
+"""returns the dictionary description with simple data structure
+"""
 
 
 def class_to_json(obj):
