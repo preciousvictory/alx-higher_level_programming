93e54221d129743dcee0f9d363e38fdc67999227
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-01-10 21:14:41 -0800

    python

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""returns the dictionary description with simple data structure """
+
+import json
+
+
+def class_to_json(obj):
+    """
+    a function that returns the dictionary description with simple data
+    structure (list, dictionary, string, integer and boolean) for JSON
+    serialization of an object:
+
+    Agrv: 
+        obj: an instance of a Class
+
+    All attributes of the obj Class are serializable: list, dictionary,
+    string, integer and boolean
+    """
+    return obj.__dict__
+
