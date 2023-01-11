aebe7395d96dca812a1c39e6ffaefca540041426
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-01-10 20:03:37 -0800

    python;

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""
-a script that adds all arguments to a Python list, and then save them to a
-file
-"""
-
+"""all arguments to a Python list, and then save them to a file"""
 import sys
 
 if __name__ == "__main__":
@@ -11,13 +7,11 @@ if __name__ == "__main__":
             __import__('6-load_from_json_file').load_from_json_file
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-filename = "add_item.json"
+    try:
+        my_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        my_list = []
 
-try:
-    my_list = load_from_json_file(filename)
-except FileNotFoundError:
-    my_list = []
+    my_list.extend(sys.argv[1:])
 
-my_list.extend(sys.argv[1:])
-
-save_to_json_file(my_list, filename)
+    save_to_json_file(my_list, "add_item.json")
