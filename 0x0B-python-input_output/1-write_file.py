7f2ef5d7b5973b2f5cc74294dad3284e6f0c4166
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-01-10 18:00:26 -0800

    python

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+writes a string to a text file
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    a function that writes a string to a text file (UTF8) and returns the number of characters written:
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+       return f.write(text)
