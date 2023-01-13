71ae1c60a6b110b57c2cae60a8374245cc041292
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-01-12 23:56:08 -0800

    python

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -10,26 +10,24 @@ def print_status():
     Printing the status of the request
     """
     count = 0
-    status_code = {"200" : 0, "301" : 0, "400" : 0, "401" : 0, "403" : 0,\
-            "404" : 0, "405" : 0, "500" : 0}
+    status_code = {"200": 0, "301": 0, "400": 0, "401": 0,
+                   "403": 0, "404": 0, "405": 0, "500": 0}
     size = 0
 
-    for l in sys.stdin:
-        line = l.split()
-        
+    for input_ in sys.stdin:
+        line = input_.split()
+
         try:
             status = line[-2]
             size += int(line[-1])
             status_code[status] += 1
-        except:
-            continue
 
         if count == 9:
             print("File size: {}".format(size))
             for key, value in sorted(status_code.items()):
                 if value != 0:
                     print(key, ":", value)
-            count = 0  
+            count = 0
         count += 1
 
     if count < 9:
@@ -37,6 +35,7 @@ def print_status():
         for key, value in sorted(status_code.item()):
             if value != 0:
                 print(key, ":", value)
+
 
 if __name__ == "__main__":
     print_status()
