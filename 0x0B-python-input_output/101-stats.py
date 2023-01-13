a176ec461ead3c1a57c7f26ab6b7267efa197686
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-01-12 23:19:38 -0800

    python

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+a script that reads stdin line by line and computes metrics
+"""
+import sys
+
+
+def print_status():
+    """
+    Printing the status of the request
+    """
+    status_code = {"200" : 0, "301" : 0, "400" : 0, "401" : 0, "403" : 0,
+                    "404" : 0, "405" : 0, "500" : 0}
+    count, size = 0, 0
+
+    for l in sys.stdin:
+        line = l.split()
+        
+        try:
+            status = line[-2]
+            size += line[-1]
+            status_code[status] += 1
+        except:
+            continue
+
+        if count == 9:
+            print("File size: {}".format(size))
+            for key, value in sorted(status_code.item()):
+                if value != 0:
+                    print(key, ":", value)
+            count = 0  
+        count += 1
+
+    if count < 9:
+        print("File size: {}".format(size))
+        for key, value in sorted(status_code.item()):
+            if value != 0:
+                print(key, ":", value)
+
+if __name__ == "__main__":
+    print_status()
