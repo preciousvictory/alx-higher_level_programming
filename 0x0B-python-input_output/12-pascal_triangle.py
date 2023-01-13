efa3e9d64530def347053b505a45e2ccd55ab2d6
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-01-12 18:58:39 -0800

    python

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Pascal's Triangle - returns a list of lists of integers representing the
+Pascal’s triangle of n
+"""
+
+
+def pascal_triangle(n):
+    """
+    a function def pascal_triangle(n): that returns a list of lists of
+    integers representing the Pascal’s triangle of n:
+    """
+    if n < 0:
+        return []
+
+    pas_tri = []
+    for i in range(n):
+        nth_list = []
+        pas_tri.append(nth_list)
+        nth_list.append(1)
+        for j in range(1, i):
+            nth_list.append(pas_tri[i - 1][j - 1] + pas_tri[i - 1][j])
+        
+        if i != 0:
+            nth_list.append(1)
+
+    return pas_tri
