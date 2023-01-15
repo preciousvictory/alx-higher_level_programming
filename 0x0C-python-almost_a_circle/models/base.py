530128a37ec127582d4f6fd890a94e1b5ef9808b
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-01-15 08:38:29 -0800

    python

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+create the Base class
+"""
+
+
+class Base:
+    """
+    the first class Base
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else: 
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
