5550a57c51f8ca879e76718477b4594afe1d1d41
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-01-16 17:46:36 -0800

    python

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -29,3 +29,22 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+         class methodthat writes the JSON string representation of
+         list_objs to a file:
+         The filename must be: <Class name>.json - example: Rectangle.json
+        """
+        filename = "{}.json".format(cls.__name__)
+        text = []
+
+        if  list_objs is not None:
+            for r in list_objs:
+                r = r.to_dictionary()
+                json_dict = json.loads(cls.to_json_string(r))
+                text.append(json_dict)
+
+        with open(filename, 'w', encoding="utf-8") as f:
+            json.dump(text, f)
