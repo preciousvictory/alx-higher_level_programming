35ad58af3d5cf6ac41cb19e0ae077d2d5345b258
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-01-17 16:49:20 -0800

    python;

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,3 +1,42 @@
 #!/usr/bin/python3
+import os
 import unittest
 from models.base import Base
+
+class TestBase(unittest.TestCase):
+    """Test class - Base"""
+    def test_instance(self):
+        """tests to check instance"""
+        r1 = Base()
+        self.assertTrue(isinstance(r1, Base))
+
+    def test_attribute(self):
+        """Test if it has all required attributes"""
+        self.assertTrue(hasattr(Base, "_Base__nb_objects"))
+
+        self.assertEqual(getattr(Base, "_Base__nb_objects"), 0)
+
+    def test_id(self):
+        """Test id"""
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+
+        b2 = Base()
+        b3 = Base()
+        self.assertEqual(b3.id, 3)
+        self.assertEqual(b2.id, 2)
+
+        c = 0
+        for i in range(5):
+            Base()
+            c += 1
+
+        b4 = Base()
+        self.assertEqual(b4.id, 3 + c + 1)
+
+        b5 = Base(12)
+        self.assertEqual(b5.id, 12)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -135,6 +135,39 @@ class TestRectangle(unittest.TestCase):
             print(r1)
             self.assertEqual(f_out.getvalue(), p)
 
+    def test_update(self):
+        """Test the update method"""
+        r1 = Rectangle(10, 10, 10, 10)
+        
+        r1.update(89)
+        self.assertEqual(r1.id, 89)
+
+        r1.update(9, 2)
+        self.assertEqual(r1.id, 9)
+        self.assertEqual(r1.width, 2)
+
+        r1.update(89, 2, 3)
+        self.assertEqual(r1.height, 3)
+
+        r1.update(89, 2, 3, 4)
+        self.assertEqual(r1.x, 4)
+
+        r1.update(89, 2, 3, 4, 5)
+        self.assertEqual(r1.y, 5)
+
+        r1.update(height=1)
+        self.assertEqual(r1.height, 1)
+
+        r1.update(width=1, x=2)
+        self.assertEqual(r1.width, 1)
+        self.assertEqual(r1.x, 2)
+
+        r1.update(y=1, width=2, x=3, id=89)
+        self.assertEqual(r1.y, 1)
+        self.assertEqual(r1.width, 2)
+        self.assertEqual(r1.x, 3)
+        self.assertEqual(r1.id, 89)
+
 
 if __name__ == '__main__':
     unittest.main()
