6a80b5e5ec56ac3edceaa3eb4ffe3fb2655e3f5a
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-01-20 12:19:47 -0800

    sortingalgorithm

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -44,6 +44,11 @@ class TestBase(unittest.TestCase):
 
     def test_save_to_file(self):
         """test method sate_to_file"""
+        r = Rectangle(1, 2)
+        Rectangle.save_to_file([r])
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual(type(f.read()), str)
+
         r = Rectangle(10, 7, 2, 8, 5)
         Rectangle.save_to_file([r])
         with open("Rectangle.json", "r") as f:
@@ -106,6 +111,26 @@ class TestBase(unittest.TestCase):
         self.assertIsNot(s1, s2)
         self.assertNotEqual(s1, s2)
 
+    def test_load_from_file(self):
+        """Test load_from_file"""
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        list_rectangles_input = [r1, r2]
+        
+        Rectangle.save_to_file(list_rectangles_input)
+        list_rectangles_output = Rectangle.load_from_file()
+
+        self.assertIsNot(list_rectangles_input[0], list_rectangles_output[0])
+        self.assertIsNot(list_rectangles_input[1], list_rectangles_output[1])
+
+        s1 = Square(5)
+        s2 = Square(7, 9, 1)
+        list_squares_input = [s1, s2]
+
+        Square.save_to_file(list_squares_input)
+        list_squares_output = Square.load_from_file()
+        self.assertIsNot(list_rectangles_input[0], list_rectangles_output[0])
+        self.assertIsNot(list_rectangles_input[1], list_rectangles_output[1])
 
 
 if __name__ == '__main__':
