b024265ab76af4803fb03412ab095c3dc1a49a47
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-13 14:34:05 -0700

    javascript is amazing

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 const args = process.argv;
-if (args.length === 3) {
+if (args.length <= 3) {
   console.log('0');
-} else if (args.length > 3) {
+} else {
   args.splice(0, 2);
   args.sort().reverse();
   console.log(args[1]);
-} else {
-  console.log('0');
 }
