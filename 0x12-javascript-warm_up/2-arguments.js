5c123d8aa929488f1183eebca46d1ed152036499
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-13 08:32:26 -0700

    javascript is amazing

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,15 +1,9 @@
 #!/usr/bin/node
-let arguments = process.argv;
-
-if (arguments.length > 3)
-{
-	console.log('Arguments found');
-}
-else if (arguments.length === 3)
-{
-	console.log('Argument found');
-}
-else
-{
-	console.log('No argument');
+const args = process.argv;
+if (args.length > 3) {
+  console.log('Arguments found');
+} else if (args.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
 }
