f4833328b96ec35a5b3c6c6d3e0b30b6f9f6f26a
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-13 11:10:56 -0700

    javascript is amazing

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const args = parseInt(process.argv[2]);
 if (args) {
-  for (i = 0; i < args; i++){
+  for (let i = 0; i < args; i++) {
     console.log('C is fun');
   }
 } else {
-	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 }
