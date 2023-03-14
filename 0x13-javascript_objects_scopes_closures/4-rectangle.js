5dd51b8b3726a0f3e1372088bb18aa006e1d9f69
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-14 14:20:22 -0700

    class

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -12,6 +12,17 @@ class Rectangle {
       console.log('X'.repeat(this.width));
     }
   }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 }
 
 module.exports = Rectangle;
