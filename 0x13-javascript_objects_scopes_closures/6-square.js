342bda6de1bd5e3a4b3432336b72543dae16adf6
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-14 15:06:07 -0700

    class

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,31 +1,6 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-
-  rotate () {
-    const temp = this.width;
-    this.width = this.height;
-    this.height = temp;
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-}
-
-class Square extends Rectangle {
+const square = require('./5-square');
+class Square extends square {
   constructor (size) {
     super();
     this.height = size;
@@ -44,5 +19,4 @@ class Square extends Rectangle {
   }
 }
 
-module.exports = Rectangle;
 module.exports = Square;
