899c6c5e63262b73f677332c856ed4b68c281abf
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-17 06:46:21 -0700

    mysqldb

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
 """
+a script that takes in arguments and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument&is safe from MySQL injections
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv
+    user = argv[1]
+    passwd = argv[2]
+    db_name = argv[3]
+    state = argv[4]
+    db = MySQLdb.connect(host="localhost", user=user, password=passwd,
+                         database=db_name)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name = %s", (state,))
+    q = cur.fetchall()
+    for i in q:
+        print(i)
