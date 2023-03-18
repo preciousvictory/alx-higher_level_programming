0e3f4d8781ceb56d6fa8fc45427b974b3cdf6d95
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-17 23:12:19 -0700

    sqlalchemy

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,2 +1,25 @@
 #!/usr/bin/python3
 """
+a script that changes the name of a State object fromthedatabase hbtn_0e_6_usa
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv
+    user = argv[1]
+    passwd = argv[2]
+    db_name = argv[3]
+    db_url = f'mysql+mysqldb://{user}:{passwd}@localhost:3306/{db_name}'
+
+    engine = create_engine(db_url, echo=False)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    r = session.query(State).get(2)
+
+    r.name = 'New Mexico'
+    session.commit()
