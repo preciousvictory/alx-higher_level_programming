3f45dbaf5ca9f12a4298272452b07cea3bcc7d9e
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-30 15:58:52 -0700

    network

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
-#!bin/bash
+#!/bin/bash
 # a Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
 curl -d "X-School-User-Id=98" -sL "$1"
