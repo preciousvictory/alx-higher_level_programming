034590334f30c2ae14a391936dcfcffded056f56
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-30 18:42:55 -0700

    network

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash#
 # a Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response
-curl -sw '%{http_code}' -o /dev/null "$1"
+curl -sLw "%{http_code}" -o /dev/null "$1"
