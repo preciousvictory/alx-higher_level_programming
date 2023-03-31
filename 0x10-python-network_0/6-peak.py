52314103bc16f0d6d6d231e02d27a4e2f4fbe9f0
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-30 18:23:39 -0700

    network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -19,7 +19,8 @@ def find_peak(list_):
     peak = []
     for i in range(0, len_):
         val = list_[i]
-        if i > 0 and i < len_ - 1 and list_[i - 1] <= val and list_[i + 1] <= val:
+        if i > 0 and i < len_ - 1 and list_[i - 1] <= val\
+                and list_[i + 1] <= val:
             peak.append(val)
 
     return max(peak)
