e57317ba8880beeaa9be6726d4fb995cb16a63ee
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-31 16:56:05 -0700

    network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         resp = res.read()
-        print('Body response:$')
-        print(f'\t- typ3: {type(resp)}$')
-        print(f'\t- content: {resp}$')
-        print(f'\t- utf8 content: {resp.decode("utf-8")}$')
+        print('Body response:')
+        print(f'\t- typ3: {type(resp)}')
+        print(f'\t- content: {resp}')
+        print(f'\t- utf8 content: {resp.decode("utf-8")}')
