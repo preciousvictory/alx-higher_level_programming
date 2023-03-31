a21aec545d3bc8d81f3b6510d7ced12d1e0a4076
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-30 18:16:12 -0700

    network

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 """find_peak"""
 
+
 def find_peak(list_):
     """a function that finds a peak in a list of unsorted integers."""
-    l = len(list_)
+    len_ = len(list_)
 
-    if l == 0:
+    if len_ == 0:
         return None
-    if l== 1:
+    if len_ == 1:
         return list_[0]
-    if l == 2:
+    if len_ == 2:
         if list_[0] >= list_[1]:
             return list_[0]
         elif list_[1] >= list_[0]:
             return list_[1]
 
     peak = []
-    for i in range(0, l):
+    for i in range(0, len_):
         val = list_[i]
-        if i > 0 and i < l - 1 and list_[i - 1] <= val and list_[i + 1] <= val:
+        if i > 0 and i < len_ - 1 and list_[i - 1] <= val and list_[i + 1] <= val:
             peak.append(val)
-            print(val)
 
     return max(peak)
