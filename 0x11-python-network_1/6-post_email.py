851668e318c1d01633de9f0978b3144cd7295911
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-31 18:29:42 -0700

    network

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -2,13 +2,11 @@
 """a Python script that takes in a URL and an email, sends a POST request to
 the passed URL with the email as a parameter, and displays the body of the
 response"""
+import requests
+import sys
 
 
-if __name__ == "__main__":
-    import requests
-    import sys
-
+if __name__ == '__main__':
     payload = {"email": sys.argv[2])
-
     r = requests.post(sys.argv[1], data=payload)
     print(r.text)
