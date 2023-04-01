76cecb659d09c1539bf1aade2d16c8f6dca56ae8
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-31 19:00:20 -0700

    network

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,8 +6,8 @@ import requests
 
 
 if __name__ == "__main__":
-    try:
-        r = requests.get(sys.argv[1])
+    r = requests.get(sys.argv[1])
+    if r.status_code < 400:
         print(r.text)
-    except requests.exceptions.HTTPError as err:
-        print('Error code: {}'.format(err))
+    else:
+        print('Error code: {}'.format(r.status_code))
