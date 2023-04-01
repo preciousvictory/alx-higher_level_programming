d289605762c484a5d7c406c7b82746f4523d66da
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-31 18:01:20 -0700

    pnetwork

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     import sys
 
     try:
-        with urllib.request.urlopen(sys.argv[1]) as resp:
+        with request.urlopen(sys.argv[1]) as resp:
             print(resp.read().decode("utf-8"))
 
-    except urllib.error.HTTPError as e:
+    except error.HTTPError as e:
         print('Error code: {}'.format(e.code))
