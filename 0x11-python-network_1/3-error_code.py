c1424b735a17996f254e04b4ad5a1e476eabda4e
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-31 17:52:54 -0700

    pnetwork

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,14 +4,12 @@ the body of the response (decoded in utf-8)."""
 
 
 if __name__ == "__main__":
-    import urllib.request
-    import urllib.error
+    from urllib import request, error
     import sys
 
-    req = urllib.request.Request(sys.argv[1])
     try:
-        with urllib.request.urlopen(req) as resp
-        resp.read().decode("utf-8")
+        with urllib.request.urlopen(sys.argv[1]) as resp:
+            print(resp.read().decode("utf-8"))
 
     except urllib.error.HTTPError as e:
         print('Error code: {}'.format(e.code))
