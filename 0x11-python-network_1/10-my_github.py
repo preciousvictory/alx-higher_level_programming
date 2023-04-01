51b259bbff7fee281ed8a9f8e831b205248e6389
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-31 19:45:50 -0700

    network

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a Python script that takes your GitHub credentials (username and password
+and uses the GitHub API to display your id"""
+import requests
+from requests.auth import HTTPBasicAuth
+import sys
+
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    url = 'https://api.github.com/users/{}'.format(argv[1])
+
+    response = requests.get(uri, auth=(user, passwd))
+    print(reponse.read().get('id')
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -19,6 +19,6 @@ if __name__ == '__main__':
         if len(res) == 0:
             print("No result")
         else:
-            print('[{}] {}'.format(res["id"], res["name"]))
+            print('[{}] {}'.format(res.get("id"), res.get("name")))
     except:
         print("Not a valid JSON")
