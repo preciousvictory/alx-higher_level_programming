a46a65bb25c821afcd08e2870668beb49475cfed
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-31 19:27:37 -0700

    network

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,3 +1,24 @@
 #!/usr/bin/python3
 """ a Python script that takes in a letter and sends a POST request to
 http://0.0.0.0:5000/search_user with the letter as a parameter."""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    if sys.arv[1]:
+        q = sys.argv[1]
+    else:
+        q = ""
+
+    payload = {"q": q}
+    r = requests.post("http://0.0.0.0:5000/search_user", json=payload)
+
+    try:
+        res = r.json()
+        if len(res) == 0:
+            print("No result")
+        else:
+            print('[{}] {}'.format(res["id"], res["name"]))
+    except:
+        print("Not a valid JSON")
