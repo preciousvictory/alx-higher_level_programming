719f71b3eea6aa9a9656409eafa0be359db3c518
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-03-31 19:48:28 -0700

    network

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,7 +9,7 @@ import sys
 if __name__ == '__main__':
     user = sys.argv[1]
     passwd = sys.argv[2]
-    url = 'https://api.github.com/users/{}'.format(argv[1])
+    url = 'https://api.github.com/users/{}'.format(user])
 
     response = requests.get(uri, auth=(user, passwd))
-    print(reponse.read().get('id')
+    print(reponse.json().get('id')
