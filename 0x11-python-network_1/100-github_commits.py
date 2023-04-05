e73b56adb8c2254a9b0d08d337c748df9f72bf91
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-04-04 18:01:51 -0700

    network

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 """
-a Python script that  Please list 10 commits (from the most recent to
-oldest) of the repository by the user """
+list 10 commits (from the most recent to oldest) of the repository
+"""
 import requests
 from sys import argv
 
-
 if __name__ == '__main__':
-    url = "https://api.github.com/repos/{}/{}/commits"
-    .format(argv[2], argv[1])
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
     res = requests.get(url)
-    commits = res.json()
-    for res in commits[:10]:
-        print('{}: {}'
-              .format(res.get('sha'), res.get('commit').get('author').get('name')))
+    commit = res.json()
+    for r in commit[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
