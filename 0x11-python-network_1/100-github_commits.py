3f603c8b8387768425619dedb2d45979246f05db
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-04-04 17:35:22 -0700

    network

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 """ a Python script that  Please list 10 commits (from the most recent to
 oldest) of the repository “rails” by the user “rails”"""
+from requests.auth import HTTPBasicAuth
 import requests
 import sys
 
 
 if __name__ == '__main__':
-    url = 'https://api.github.com/users/{}/{}'.format(sys.argv[1])
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    res = requests.get(url, auth=auth)
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(sys.argv[2],sys.argv[1])
+    commit = requests.get(url).json()
+    i = 0
+    for res in commit:
+        if i < 10:
+            print('{}: '.format(res['sha'], res.get('commit').get('author').get('name')))
+        else:
+            break
+        i += 1
