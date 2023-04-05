1cbf646f5499cfbcd795dfc5d798484d642dc88c
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-04-04 17:53:45 -0700

    list all github repo commit

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,7 +8,7 @@ from sys import argv
 
 
 if __name__ == '__main__':
-    url = 'https://api.github.com/repos/{}/{}/commits'
+    url = "https://api.github.com/repos/{}/{}/commits"
     .format(argv[2], argv[1])
     res = requests.get(url)
     commits = res.json()
