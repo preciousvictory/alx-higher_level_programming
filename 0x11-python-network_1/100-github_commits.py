dd8590a6b4973128f6d4917490c603f730e6c022
Abiodun-Omoniyi Victory <victoryexcel2018@gmail.com>
2023-04-04 18:39:59 -0700

    list all commit

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,12 +5,11 @@ list 10 commits (from the most recent to oldest) of the repository
 import requests
 from sys import argv
 
-if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits"\
-            .format(argv[2], argv[1])
+if __name__ == '__main__':
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(
+          argv[2], argv[1])
     res = requests.get(url)
     commits = res.json()
-
-    for r in commits:
-        print(r.get('sha'), end=': ')
-        print(r.get('commit').get('author').get('name'))
+    for res in commits[:10]:
+        print('{}: {}'.format(
+            res.get('sha'), res.get('commit').get('author').get('name')))
