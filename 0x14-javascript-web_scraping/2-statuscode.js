09c8ab0f45f2dce98684484b561557ae3caba58d
preciousvictory <victoryexcel2018@gmail.com>
2023-04-25 14:34:32 -0700

    javascript

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,6 +4,6 @@
 const request = require('request');
 request
   .get(process.argv[2])
-  .on('response', function(response) {
-    console.log('code: ' + response.statusCode)
+  .on('response', function (response) {
+    console.log('code: ' + response.statusCode);
   });
