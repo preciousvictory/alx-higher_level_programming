a58d1319c56c3a07190a09bd459f2476bc67de5a
preciousvictory <victoryexcel2018@gmail.com>
2023-04-25 14:04:10 -0700

    javascript

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 /* a script that reads and prints the content of a file. */
 
-let filename = process.argv[2]
-let fs = require("fs");
+const filename = process.argv[2];
+const fs = require('fs');
 
 fs.readFile(filename, 'utf8', function (err, data) {
-      if (err) {
-        return console.error(err);
-      }
-      console.log(data.toString());
+  if (err) {
+    return console.error(err);
+  }
+  console.log(data.toString());
 });
