2d7e5dd151a03aef281c45ec6270ed895606421b
preciousvictory <victoryexcel2018@gmail.com>
2023-07-27 15:38:57 -0700

    python

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -3,3 +3,5 @@ print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_
 
 my_list = [1, 2, 3, 4, 5]
 print_reversed_list_integer(my_list)
+my_lists = [1, 2, 'H', 9]
+print_reversed_list_integer(my_lists)
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,7 @@
 def print_reversed_list_integer(my_list=[]):
     """
     a function that prints all integers of a list, in reverse order."""
+    if my_list is None:
+        return
     for i in range(len(my_list), 0, -1):
         print("{:d}".format(i))
