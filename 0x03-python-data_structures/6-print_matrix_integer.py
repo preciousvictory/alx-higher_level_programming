971b0de1a3f508e4305749f51e8ee4e77d6939fc
preciousvictory <victoryexcel2018@gmail.com>
2023-07-28 04:18:42 -0700

    python

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,10 +2,4 @@
 def print_matrix_integer(matrix=[[]]):
     """a function that prints a matrix of integer"""
     for i in matrix:
-        for j in range(len(i) + 1):
-            if j % 2 != 0:
-                i.insert(j, " ")
-        txt = ""
-        for j in i: 
-            txt += '{}'.format(j)
-        print(txt)
+        print(' '.join('{:d}'.format(j) for j in i))
