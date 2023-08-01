d5f320838ebf46f2d8de3d6b476e00dd079e3749
preciousvictory <victoryexcel2018@gmail.com>
2023-08-01 16:18:04 -0700

    python

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is False:
+    if len(my_list) == 0:
         return None
-    big = 0
+    big = my_list[0]
     for i in my_list:
         if i > big:
             big = i
