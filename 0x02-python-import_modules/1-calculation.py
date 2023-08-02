4f61744be538117adaea6a2815a23c7b36a44b75
preciousvictory <victoryexcel2018@gmail.com>
2023-08-02 14:02:26 -0700

    python

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+    print("{a} + {b} = {ans}".format(a = a, b = b, ans = add(a, b)))
+    print("{a} - {b} = {ans}".format(a = a, b = b, ans = sub(a, b)))
+    print("{a} * {b} = {ans}".format(a = a, b = b, ans = mul(a, b)))
+    print("{a} / {b} = {ans}".format(a = a, b = b, ans = div(a, b)))
