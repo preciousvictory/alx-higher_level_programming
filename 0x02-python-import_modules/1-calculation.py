f409013001d2dbdef7d4500bcde61445eaa99daf
preciousvictory <victoryexcel2018@gmail.com>
2023-08-02 14:06:56 -0700

    python

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
 
     a = 10
     b = 5
-    print("{a} + {b} = {ans}".format(a = a, b = b, ans = add(a, b)))
-    print("{a} - {b} = {ans}".format(a = a, b = b, ans = sub(a, b)))
-    print("{a} * {b} = {ans}".format(a = a, b = b, ans = mul(a, b)))
-    print("{a} / {b} = {ans}".format(a = a, b = b, ans = div(a, b)))
+    print("{a} + {b} = {ans}".format(a=a, b=b, ans=add(a, b)))
+    print("{a} - {b} = {ans}".format(a=a, b=b, ans=sub(a, b)))
+    print("{a} * {b} = {ans}".format(a=a, b=b, ans=mul(a, b)))
+    print("{a} / {b} = {ans}".format(a=a, b=b, ans=div(a, b)))
