1f513b8292a888df3ad1dabecfba101399222041
preciousvictory <victoryexcel2018@gmail.com>
2023-08-05 15:33:54 -0700

    python

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    """returns the weighted average of all integers tuple (<score>, <weight>)
+    my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
+    ans = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)"""
+    if not my_list:
+        return 0
+    
+    scores = 0
+    weight = 0
+    for grade in my_list:
+        scores += (grade[0] * grade[1])
+        weight += grade[1]
+    return scores / weight
