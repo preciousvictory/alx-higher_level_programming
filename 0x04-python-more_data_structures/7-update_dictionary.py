7ad337601f95ce4009ea5cc93d52ac0f22c70923
preciousvictory <victoryexcel2018@gmail.com>
2023-08-05 13:47:55 -0700

    python

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """replaces or adds key/value in a dictionaryi"""
+    a_dictionary.update({key: value})
+    return a_dictionary
