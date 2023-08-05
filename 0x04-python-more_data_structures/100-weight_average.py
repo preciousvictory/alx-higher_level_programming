14ebf6402a3be1bd3a882034261f70d290654ada
preciousvictory <victoryexcel2018@gmail.com>
2023-08-05 15:35:30 -0700

    python

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,7 +5,7 @@ def weight_average(my_list=[]):
     ans = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)"""
     if not my_list:
         return 0
-    
+
     scores = 0
     weight = 0
     for grade in my_list:
