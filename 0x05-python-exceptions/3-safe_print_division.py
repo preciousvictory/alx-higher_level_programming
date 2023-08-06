45bda5d7c38660c229127eeaee98935fe3bff65d
preciousvictory <victoryexcel2018@gmail.com>
2023-08-06 10:15:12 -0700

    python

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    """ divides 2 integers and prints the result."""
+    try:
+        result = a / b
+    except Exception as e:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    
+    return result
